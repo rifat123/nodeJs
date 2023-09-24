ad386ad23390c0b520b37a7cf7e9135d68f95f61
Rifat <rifat.hasan55511@gmail.com>
2023-09-24 15:00:14 -0500

    test2

--- 5-utiils.js
+++ 5-utiils.js
@@ -1,4 +1,29 @@
-const _ =require('loadsh')
-const items=[1,[1,2,3,4]]
-const newItems=_.flattenDeep(items)
-console.log(newItems)
+// const _ =require('loadsh')
+// const items=[1,[1,2,3,4]]
+// const newItems=_.flattenDeep(items)
+// console.log(newItems)
+
+
+console.log("test")
+
+
+// async function fetchData() {
+//     try {
+//       const response = await fetch('https://api.example.com/data');
+//       if (!response.ok) {
+//         throw new Error('Failed to fetch data');
+//       }
+      
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       console.error('Error:', error);
+//       throw error;
+//     }
+//   }
+
+const EventEmitter=require('events')
+
+  
+const customEmitter=new EventEmitter()
+customEmitter.on('response')
